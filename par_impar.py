14efbd4ae4a453a68c6f6866b9866fcd016cec23
JorgePascualGracia2001 <43232543+JorgePascualGracia2001@users.noreply.github.com>
2020-09-24 11:32:52 +0200

    Delete par_impar.py

--- par_impar.py
+++
@@ -1,8 +0,0 @@
-def par_impar():
-    numero=input("Introzuce un numero entero, inútil de mierda: ")
-    if(numero%2==0):
-        print "PAR, BURRO INCULTO"
-    else:
-        print "IMPAR, COMO SE NOTA QUE ERES DE ARTES"
-        
-par_impar()
